5682f64315e5fd9fab006a3d90c31a2ccf115997
Matti <m.al-hussan@lyon.ort.asso.fr>
2025-12-08 16:40:58 +0100

    Debut

---
+++ src/Controller/indexController.php
@@ -0,0 +1,32 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+#[Route(path : '/')]
+class indexController extends AbstractController
+{
+   
+
+
+    #[Route(path: '/hello/{name}')]
+
+    public function hello(Request $request, string $name): Response
+    {
+        dump ($request->query->get('module'));
+
+
+        return $this->render(
+
+        view : 'index.html.twig',
+        
+        parameters : [
+            'name' => $name
+        ]
+    );
+    }
+}
